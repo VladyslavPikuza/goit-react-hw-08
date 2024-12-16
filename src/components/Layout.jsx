1563780449a5472075e0f2043237927d3bace1ea
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-12-16 11:06:34 +0200

    changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import { AppBar } from './AppBar/AppBar';
 
 export const Layout = () => {
   return (
-    <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
+    <div>
       <AppBar />
       <Suspense fallback={null}>
         <Outlet />
